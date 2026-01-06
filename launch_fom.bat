e953fa7fc40f0c71655fba207da8353addb9fa5d
BioXide <arolsouls@gmail.com>
2026-01-06 17:00:12 -0500

    dropped default res to have more screen space

--- launch_fom.bat
+++ launch_fom.bat
@@ -7,6 +7,6 @@ echo Using same command line as FOMLauncher.exe...
 echo.
 
 REM This is the EXACT command line from FOMLauncher.exe
-start "" "fom_client.exe" -rez Resources -config master.cfg -dpsmagic 1234 +windowed 0 +EnableTripBuf 1 +ScreenWidth 1920 +ScreenHeight 1080 +ConsoleEnable 1 +windowed 1
+start "" "fom_client.exe" -rez Resources -config master.cfg -dpsmagic 1234 +windowed 0 +EnableTripBuf 1 +ScreenWidth 1024 +ScreenHeight 768 +ConsoleEnable 1 +windowed 1
 
 exit /b
