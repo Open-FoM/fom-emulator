e0e8b9270c37b3400c0c7f74d5bd333024854874
BioXide <arolsouls@gmail.com>
2026-01-15 22:04:54 -0500

    Open-FoM level editor

---
+++ Open-FoMEdit/unrez.bat
@@ -0,0 +1,11 @@
+@echo off
+
+@echo rem ===================================
+@echo rem === Un-Rez the NOLF2 Resources   ===
+@echo rem ===================================
+
+lithrez xv ..\..\GAME.rez			..\..\Game
+lithrez xv ..\..\GAME2.rez			..\..\Game
+lithrez xv ..\..\GAMEDLL.rez			..\..\Game
+lithrez xv ..\..\SOUND.rez			..\..\Game
+lithrez xv ..\..\Update_v1x3.rez		..\..\Game
